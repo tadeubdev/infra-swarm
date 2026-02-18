65962b628613e48fcb39e8a25e06f4e75e2641ab
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2026-02-18 20:14:01 -0300

    feat: add app node express

---
+++ apps/api/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json({
+    limit: "10mb",
+}));
+
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+    service: "api",
+    message: "hello from swarm + traefik",
+    time: new Date().toISOString(),
+  });
+});
+
+app.get("/error", (req, res) => {
+    res.status(500).json({
+        ok: false,
+        service: "api",
+        message: "this is an error endpoint",
+        time: new Date().toISOString(),
+    });
+});
+
+app.get("/slow", (req, res) => {
+    setTimeout(() => {
+        res.json({
+            ok: true,
+            service: "api",
+            message: "this is a slow endpoint",
+            time: new Date().toISOString(),
+        });
+    }, 5000);
+});
+
+app.get("/json", (req, res) => {
+    res.json({
+        ok: true,
+        service: "api",
+        message: "this is a json endpoint",
+        time: new Date().toISOString(),
+    });
+});
+
+app.get("/text", (req, res) => {
+    res.send("this is a text endpoint");
+});
+
+app.get("/health", (req, res) => res.status(200).send("ok"));
+
+app.listen(3000, () => console.log("API listening on :3000"));
