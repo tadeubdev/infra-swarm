55c96c30e3a980655a8d37e218131db87aee70c2
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2026-02-18 21:00:35 -0300

    feat: update App.vue to enhance layout and styling with Vue 3 + Vite + TypeScript

--- apps/app/src/App.vue
+++ apps/app/src/App.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
-    <p>Hello World!</p>
+  <div id="app">
+    <h1>Vue 3 + Vite + TypeScript</h1>
   </div>
 </template>
 
 <script setup>
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
